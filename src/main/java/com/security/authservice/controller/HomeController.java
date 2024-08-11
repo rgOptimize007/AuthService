61b462f3123bcc8555e08520a15085aa6e15d9d5
Rohit <rohitgharge2@gmail.com>
2024-08-11 12:45:06 +0530

    Second commit : updating home controller end-point

--- src/main/java/com/security/authservice/controller/HomeController.java
+++ src/main/java/com/security/authservice/controller/HomeController.java
@@ -1,14 +1,13 @@
 package com.security.authservice.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
 
-    @RequestMapping(value = "/home", method = RequestMethod.GET)
+    @GetMapping("/home")
     public String getHome(){
         return "This is a home page";
     }
